0ace6474b4bf92d2523381c372a683c5dc438639
Bensolve <54053468+Benjamin569@users.noreply.github.com>
2024-03-12 09:18:21 +0000

    just added admin template

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -21,3 +21,18 @@ export async function GET(request: Request) {
     }
 }
  
+export async function POST(request: Request) {
+    
+}
+
+export async function PUT(request: Request) {
+    
+}
+
+export async function PATCH(request: Request) {
+    
+}
+
+export async function DELETE(request: Request) {
+    
+}
