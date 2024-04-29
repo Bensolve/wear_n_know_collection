caa9ba6e81d7ffdf9b8a69de408db4c35c6d9a7f
Bensolve <54053468+Benjamin569@users.noreply.github.com>
2024-04-29 05:29:38 +0000

    changes

--- app/dashboard/add/page.jsx
+++ app/dashboard/add/page.jsx
@@ -1,21 +1,11 @@
-import Image from "next/image"
+
 import Link from "next/link"
 import {
     ChevronLeft,
-    Home,
-    LineChart,
-    Package,
-    Package2,
-    PanelLeft,
-    PlusCircle,
-    Search,
-    Settings,
-    ShoppingCart,
-    Upload,
-    Users2,
+ 
 } from "lucide-react"
 
-import { Badge } from "@/components/ui/badge"
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -29,38 +19,14 @@ import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
+  
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-
-
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-    TooltipProvider
-} from "@/components/ui/tooltip"
 
 import { addProduct } from "@/app/lib/actions";
 
